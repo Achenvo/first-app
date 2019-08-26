713b6e968fe19f8dd24945ad9b07dad92b51de8d
aaa <dddd>
2019-08-26 18:15:44 +0800

    dddd

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 80px;
-  padding: 10px;
-  background-color: #fff;
+
+
 }
 </style>
